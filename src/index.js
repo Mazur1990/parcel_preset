7da97fcdc99d11984d0860588789473bf5b69a9f
Mazur1990 <and.mazur5@gmail.com>
2020-04-16 20:29:45 +0200

    Page rendering by JS

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import './styles/main.scss'
 
 //Java Script import (saparate this section to Views and Logics)
 //Views
-import {viewSampel} from './javascript_pages/view_sampel'
+import {renderSampel} from './javascript_pages/view_sampel'
 //Logics
 import {sampel} from './javascript_logics/sampel';
 sampel();
-viewSampel();
+renderSampel();
 
 
 
